c785ca958a36ead91c1d9c23bc44c2aee870cc7c
Mayank Upadhyay <mayank6220@gmail.com>
2024-08-14 21:44:47 +0530

    commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">MAYANK</div>
+          <div className="right">
+            <p>NOIDA-52, UP, INDIA</p>
+            <p>Open: 10:00 AM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Mayank Upadhyay</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Mayank Upadhyay</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
